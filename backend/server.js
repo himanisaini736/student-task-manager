b67a0928038a9a28789cad3a5b0e17eba582d3ad
Himani <himanidks@gmail.com>
2026-07-11 16:13:47 +0530

    Lesson 2: Connect MongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+console.log("My Name is " + process.env.MY_NAME);
+
+app.get("/", (req, res) => {
+    res.send("Student Task Manager Backend is Running 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
